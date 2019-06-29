723cb206418d633100f242d0705a6b850869b104
zhangsan <zhangsan@.com>
2019-06-29 20:48:26 +0800

    克隆修改上传

--- git_test/src/com/itcast/GItTest.java
+++ git_test/src/com/itcast/GItTest.java
@@ -3,31 +3,12 @@ package com.itcast;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("git 练习;");
-        System.out.println("git 练习;");
-        System.out.println("git 练习;");
-        System.out.println("git 练习;");
-        System.out.println("git 练习;");
-        System.out.println("git 练习;");
-        System.out.println("git 练习;");
-
-        System.out.println("git 练习;");
     }
     public void test1() {
-        System.out.println("练习222222222222222222222222");
-        System.out.println("练习222222222222222222222222");
-        System.out.println("练习222222222222222222222222");
-        System.out.println("练习222222222222222222222222");
-        System.out.println("练习222222222222222222222222");
-        System.out.println("练习222222222222222222222222");
         System.out.println("练习222222222222222222222222");
     }
 
     public void test2() {
-        System.out.println("练习333333333333333333333333");
-        System.out.println("练习333333333333333333333333");
-        System.out.println("练习333333333333333333333333");
-        System.out.println("练习333333333333333333333333");
-        System.out.println("练习333333333333333333333333");
         System.out.println("练习333333333333333333333333");
     }
 }
