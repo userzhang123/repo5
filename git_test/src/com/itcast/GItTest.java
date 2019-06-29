24ecf4497369c54e8e75a49c1457845ea8138173
zhangsan <zhangsan@.com>
2019-06-29 20:03:35 +0800

    idea添加练习

---
+++ git_test/src/com/itcast/GItTest.java
@@ -0,0 +1,12 @@
+package com.itcast;
+
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("git 练习;");
+        System.out.println("git 练习;");
+        System.out.println("git 练习;");
+        System.out.println("git 练习;");
+        System.out.println("git 练习;");
+        System.out.println("git 练习;");
+    }
+}
