824c5e08e1d860d82e94254bdb6b3944a03676f1
zhangsan <zhangsan@.com>
2019-06-29 21:04:29 +0800

    克隆修改上传

--- git_test/src/com/itcast/GItTest.java
+++ git_test/src/com/itcast/GItTest.java
@@ -4,33 +4,4 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("git 练习;");
     }
-    public void test1() {
-        System.out.println("练习222222222222222222222222");
-        //
-        //
-        //
-        //
-        //
-
-    }
-
-    public void test2() {
-        System.out.println("练习333333333333333333333333");
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-        //
-
-
-    }
 }
